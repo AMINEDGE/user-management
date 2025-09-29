dec5f2d1d4ad9c337216869e253c2d894a8c0e3e
Mohammad Amin Hajari <mahthenewgamer1998@icloud.com>
2025-09-29 14:56:30 +0330

    user list responsive

--- src/app/page.js
+++ src/app/page.js
@@ -25,6 +25,24 @@ export default function Home() {
   const [totalPages, setTotalPages] = useState(2);
   const [users, setUsers] = useState([]);
 
+  const [isPhone, setIsPhone] = useState(false);
+
+  const isMobile = () => window.innerWidth <= 768;
+  useEffect(() => {
+    requestAnimationFrame(() => setIsPhone(isMobile()));
+  }, []);
+  useEffect(() => {
+    const handleResize = () => {
+      requestAnimationFrame(() => setIsPhone(isMobile()));
+    };
+
+    // Initial check on mount
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const init = async () => {
     setLoadingEnabled(true);
     const savedToken = localStorage.getItem("token");
@@ -95,95 +113,173 @@ export default function Home() {
     init();
   }, [page]);
 
-  const UserList = (props) => (
-    <div className="flex flex-col items-center bg-[#f8f8f8] rounded-xl overflow-hidden p-4 my-8">
-      <table>
-        <thead>
-          <tr className="h-30">
-            <th className="w-20">Index</th>
-            <th className="w-60">Email</th>
-            <th className="w-60">Name</th>
-            <th className="w-40">Avatar</th>
-            <th className="w-20">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users?.map((u, i) => (
-            <tr key={i} className="h-30">
-              <td className="text-center">{i + 1}</td>
-              <td className="text-center">{u.email}</td>
-              <td className="text-center">{`${u.first_name} ${u.last_name}`}</td>
-              <td className="text-center">
-                <img
-                  className="mx-auto rounded-4xl w-20 h-20 object-cover"
-                  src={u.avatar}
-                />
-              </td>
-              <td className="text-center">
-                <div className="flex flex-row gap-4 self-center">
-                  <Link href={`/user/details/${u.id}`}>
-                    <Eye color="#0ac" />
-                  </Link>
-                  <Link href={`/user/edit/${u.id}`}>
-                    <Pencil color="#fc0" />
-                  </Link>
-                  <button
-                    className="cursor-pointer"
-                    onClick={() => {
-                      withReactContent(Swal).fire({
-                        title: <i>You sure you want to delete this user?</i>,
-                        icon: "error",
-                        showCancelButton: true,
-                        confirmButtonText: "Yes, delete it!",
-                        cancelButtonText: "No, cancel!",
-                        preConfirm: () => {
-                          deleteUser(u.id);
-                        },
-                      });
-                    }}
-                  >
-                    <Trash2 color="#f06" />
-                  </button>
-                </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+  const UserList = (props) =>
+    isPhone ? (
+      <div>
+        {users.map((u, i) => (
+          <div className="flex flex-col items-center bg-[#f8f8f8] rounded-xl overflow-hidden p-4 my-8">
+            <div className="flex flex-row w-full items-center gap-2">
+              <img
+                className="mx-auto rounded-4xl w-20 h-20 object-cover"
+                src={u.avatar}
+              />
+              <div className="flex flex-col w-60">
+                <label>
+                  <strong>Name: </strong>
+                  {`${u.first_name} ${u.last_name}`}
+                </label>
+                <label>
+                  <strong>Email: </strong>
+                  {`${u.email}`}
+                </label>
+              </div>
+            </div>
 
-      <div className="flex flex-row items-center gap-4">
-        <button
-          disabled={page == 1}
-          className={`rounded-full p-2 ${
-            page == 1
-              ? "bg-[#ccc] cursor-not-allowed"
-              : "bg-purple-600 cursor-pointer"
-          }`}
-          onClick={() => setPage(page - 1)}
-        >
-          <ChevronLeft className="text-white" />
-        </button>
-        <span>
-          Page {page} of {totalPages}
-        </span>
-        <button
-          disabled={page == totalPages}
-          onClick={() => setPage(page + 1)}
-          className={`rounded-full p-2 ${
-            page == totalPages
-              ? "bg-[#ccc] cursor-not-allowed"
-              : "bg-purple-600 cursor-pointer"
-          }`}
-        >
-          <ChevronRight className="text-white" />
-        </button>
+            <div className="flex flex-row gap-4 items-center justify-end">
+              <Link href={`/user/details/${u.id}`}>
+                <Eye color="#0ac" />
+              </Link>
+              <Link href={`/user/edit/${u.id}`}>
+                <Pencil color="#fc0" />
+              </Link>
+              <button
+                className="cursor-pointer"
+                onClick={() => {
+                  withReactContent(Swal).fire({
+                    title: <i>You sure you want to delete this user?</i>,
+                    icon: "error",
+                    showCancelButton: true,
+                    confirmButtonText: "Yes, delete it!",
+                    cancelButtonText: "No, cancel!",
+                    preConfirm: () => {
+                      deleteUser(u.id);
+                    },
+                  });
+                }}
+              >
+                <Trash2 color="#f06" />
+              </button>
+            </div>
+          </div>
+        ))}
+
+        <div className="flex flex-row items-center justify-center my-4 gap-4">
+          <button
+            disabled={page == 1}
+            className={`rounded-full p-2 ${
+              page == 1
+                ? "bg-[#ccc] cursor-not-allowed"
+                : "bg-purple-600 cursor-pointer"
+            }`}
+            onClick={() => setPage(page - 1)}
+          >
+            <ChevronLeft className="text-white" />
+          </button>
+          <span>
+            Page {page} of {totalPages}
+          </span>
+          <button
+            disabled={page == totalPages}
+            onClick={() => setPage(page + 1)}
+            className={`rounded-full p-2 ${
+              page == totalPages
+                ? "bg-[#ccc] cursor-not-allowed"
+                : "bg-purple-600 cursor-pointer"
+            }`}
+          >
+            <ChevronRight className="text-white" />
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    ) : (
+      <div className="flex flex-col items-center bg-[#f8f8f8] rounded-xl overflow-hidden p-4 my-8">
+        <table>
+          <thead>
+            <tr className="h-30">
+              <th className="w-20">Index</th>
+              <th className="w-60">Email</th>
+              <th className="w-60">Name</th>
+              <th className="w-40">Avatar</th>
+              <th className="w-20">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users?.map((u, i) => (
+              <tr key={i} className="h-30">
+                <td className="text-center">{i + 1}</td>
+                <td className="text-center">{u.email}</td>
+                <td className="text-center">{`${u.first_name} ${u.last_name}`}</td>
+                <td className="text-center">
+                  <img
+                    className="mx-auto rounded-4xl w-20 h-20 object-cover"
+                    src={u.avatar}
+                  />
+                </td>
+                <td className="text-center">
+                  <div className="flex flex-row gap-4 self-center">
+                    <Link href={`/user/details/${u.id}`}>
+                      <Eye color="#0ac" />
+                    </Link>
+                    <Link href={`/user/edit/${u.id}`}>
+                      <Pencil color="#fc0" />
+                    </Link>
+                    <button
+                      className="cursor-pointer"
+                      onClick={() => {
+                        withReactContent(Swal).fire({
+                          title: <i>You sure you want to delete this user?</i>,
+                          icon: "error",
+                          showCancelButton: true,
+                          confirmButtonText: "Yes, delete it!",
+                          cancelButtonText: "No, cancel!",
+                          preConfirm: () => {
+                            deleteUser(u.id);
+                          },
+                        });
+                      }}
+                    >
+                      <Trash2 color="#f06" />
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <div className="flex flex-row items-center gap-4">
+          <button
+            disabled={page == 1}
+            className={`rounded-full p-2 ${
+              page == 1
+                ? "bg-[#ccc] cursor-not-allowed"
+                : "bg-purple-600 cursor-pointer"
+            }`}
+            onClick={() => setPage(page - 1)}
+          >
+            <ChevronLeft className="text-white" />
+          </button>
+          <span>
+            Page {page} of {totalPages}
+          </span>
+          <button
+            disabled={page == totalPages}
+            onClick={() => setPage(page + 1)}
+            className={`rounded-full p-2 ${
+              page == totalPages
+                ? "bg-[#ccc] cursor-not-allowed"
+                : "bg-purple-600 cursor-pointer"
+            }`}
+          >
+            <ChevronRight className="text-white" />
+          </button>
+        </div>
+      </div>
+    );
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold mb-4">
+      <h1 className="text-4xl font-bold mb-4 text-center">
         Welcome to User Management App
       </h1>
       {loadingEnabled ? (
